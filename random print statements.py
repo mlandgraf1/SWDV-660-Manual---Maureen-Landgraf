04ae3e3e87a81fec078337016bcde2729e1fbcf9
mlandgraf1 <42742611+mlandgraf1@users.noreply.github.com>
2019-05-24 14:26:14 -0500

    Add files via upload

---
+++ random print statements.py
@@ -0,0 +1,10 @@
+from random import random
+
+number = random()
+
+if number < 0.35:
+    print("Time for hockey!")
+elif number > 0.65:
+    print("Let's go Blues!")
+elif number < 0.65 and number > 0.35:
+    print("Win the Stanley Cup!")
