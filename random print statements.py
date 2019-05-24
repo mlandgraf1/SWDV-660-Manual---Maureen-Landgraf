8344ed169750cad08868373461f7b6f73c500939
mlandgraf1 <42742611+mlandgraf1@users.noreply.github.com>
2019-05-24 15:10:17 -0500

    Update random print statements.py

--- random print statements.py
+++ random print statements.py
@@ -3,7 +3,7 @@ from random import random
 number = random()
 
 if number < 0.35:
-    print("Time for hockey!")
+    print("Time for the Stanley Cup Finals!")
 elif number > 0.65:
     print("Let's go Blues!")
 elif number < 0.65 and number > 0.35:
